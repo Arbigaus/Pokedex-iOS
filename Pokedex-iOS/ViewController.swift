9dce7fed84be18e486ad09672ddfc4363fb76192
Gerson Arbigaus <gerson87@gmail.com>
2020-08-03 21:39:54 -0300

    Initial Commit

---
+++ Pokedex-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex-iOS
+//
+//  Created by Gerson Arbigaus on 03/08/20.
+//  Copyright © 2020 Gerson Arbigaus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
